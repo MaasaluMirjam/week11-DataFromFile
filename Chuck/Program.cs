055ecacba842bf6cba3fc8107c86132e263b34aa
Opilane TTHK <opilane@tthk.ee>
2021-11-11 10:24:58 +0200

    DataFromFIle

---
+++ Chuck/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace Chuck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(RandomLineFromFile());
+            }           
+        }
+
+        private static string RandomLineFromFile()
+        {          
+            string filePath = @"C:\Users\opilane\samples\chuck.txt";
+
+            string[] dataFromFile = File.ReadAllLines(filePath);
+
+            Random rnd = new Random();
+            int randomIndex = rnd.Next(0, dataFromFile.Length);
+
+            return dataFromFile[randomIndex];
+        }
+    }
+}
---
+++ Horoscope/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+
+namespace Horoscope
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your horoscope:");
+            string userSign = Console.ReadLine();
+           
+            string output = GetHoroscope(userSign);
+            if (output.Length != 0)
+            {
+                Console.WriteLine(output);
+            }
+            else
+            {
+                Console.WriteLine("Sorry, your zodic sign not found.");
+            }
+        }
+        private static string[] ReadHoroscope()
+        {
+            string directoryPath = @"C:\Users\opilane\samples";
+            string fileName = "horoscope.txt";
+            string fullPath = $@"{directoryPath}\{fileName}";
+
+            string[] dataFromFile = File.ReadAllLines(fullPath);
+            return dataFromFile;
+        }
+        private static string GetHoroscope(string userInput)
+        {
+            string[] dataFromFile = ReadHoroscope();
+            string result = "";
+
+            foreach (string line in dataFromFile)
+            {
+                if (line.Contains(userInput))
+                {
+                    result = line;
+                    break;
+                }
+            }
+            return result;
+        }
+    }
+}
